4cce9d2e34ff3f480e747aff02a62541829b72e3
MAlvar000 <alvaro.mateocarrero@usp.ceu.es>
2024-11-10 12:36:01 +0100

    Ejercicio 13

---
+++ src/aplicacion/Principal.java
@@ -0,0 +1,11 @@
+package aplicacion;
+
+import Interfaz.InterfazUsuario;
+
+public class Principal {
+    public static void main(String[] args) {
+        InterfazUsuario interfaz = new InterfazUsuario();
+        interfaz.procesarPeticion();
+    }
+}
+
